99a550da49226232898b8107e57f7a79d4a07559
Naveen Mittal <naveenmittal04@gmail.com>
2023-07-03 14:53:49 +0530

    unicafe step3 and step4 done

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -7,34 +7,20 @@ const Button = ({handleClick, text}) => {
         </button>
     )
 }
-const App = () => {
-    // save clicks of each button to its own state
-    const [good, setGood] = useState(0)
-    const [neutral, setNeutral] = useState(0)
-    const [bad, setBad] = useState(0)
-    const [total, setTotal] = useState(0)
-    let totalScore = good-bad
-    const incrementGood = () => {
-        setGood(good + 1)
-        setTotal(total + 1)
-        totalScore += 1
+const Statistics = (params) => {
+    const [good, neutral, bad, totalScore] = params.feedback
+    let total = good + neutral + bad
+
+    if(total === 0){
+        return (
+            <div>
+                No feedback given
+            </div>
+        )
     }
-    const incrementNeutral = () => {
-        setNeutral(neutral + 1)
-        setTotal(total + 1)
-    }
-    const incrementBad = () => {
-        setBad(bad + 1)
-        setTotal(total + 1)
-        totalScore -= 1
-    }
+
     return (
         <div>
-            <h1>give feedback</h1>
-            <Button handleClick={incrementGood} text='good'/>
-            <Button handleClick={incrementNeutral} text='neutral'/>
-            <Button handleClick={incrementBad} text='bad'/>
-            <h1>statistics</h1>
             good {good}
             <br/>
             neutral {neutral}
@@ -46,6 +32,38 @@ const App = () => {
             average {total !== 0? totalScore/total: 0}
             <br/>
             positive {total !== 0? good/total*100: 0} %
+        </div>
+    )
+}
+const App = () => {
+    // save clicks of each button to its own state
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+   // const [total, setTotal] = useState(0)
+    let totalScore = good-bad
+    const incrementGood = () => {
+        setGood(good + 1)
+        //setTotal(total + 1)
+        totalScore += 1
+    }
+    const incrementNeutral = () => {
+        setNeutral(neutral + 1)
+        //setTotal(total + 1)
+    }
+    const incrementBad = () => {
+        setBad(bad + 1)
+        //setTotal(total + 1)
+        totalScore -= 1
+    }
+    return (
+        <div>
+            <h1>give feedback</h1>
+            <Button handleClick={incrementGood} text='good'/>
+            <Button handleClick={incrementNeutral} text='neutral'/>
+            <Button handleClick={incrementBad} text='bad'/>
+            <h1>statistics</h1>
+            <Statistics feedback={[good, neutral, bad, totalScore]}/>
         </div>
     )
 }
