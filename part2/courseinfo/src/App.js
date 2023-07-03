24cd3bd60ec4d71291311567ccccf17535224322
Naveen Mittal <naveenmittal04@gmail.com>
2023-07-03 19:11:06 +0530

    courseinfo step6 done

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,3 +1,36 @@
+function Header({name}) {
+    console.log('Header ', name)
+    return (
+        <h1>{name}</h1>
+    )
+}
+
+const Part = ({part}) => {
+    console.log('Part ', part)
+    return (
+        <p>{part.name} {part.exercises}</p>
+    )
+}
+
+function Content({parts}) {
+    console.log('Content ', parts)
+    return (
+        <div>
+            {parts.map(part => <Part key={part.id} part={part} />)}
+        </div>
+    )
+}
+
+const Course = ({ course }) => {
+    console.log('Course ', course)
+    return (
+        <div>
+            <Header name={course.name} />
+            <Content parts={course.parts} />
+        </div>
+    )
+}
+
 const App = () => {
     const course = {
         id: 1,
