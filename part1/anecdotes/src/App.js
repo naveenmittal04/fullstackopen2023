8daaed6e692bf04086c8fccfa25d4a2f24300385
Naveen Mittal <naveenmittal04@gmail.com>
2023-07-03 15:29:03 +0530

    anecdote step1 done

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
 const App = () => {
-  const anecdotes = [
+    const anecdotes = [
     'If it hurts, do it more often.',
     'Adding manpower to a late software project makes it later!',
     'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
@@ -10,15 +10,25 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
-  ]
+    ]
 
-  const [selected, setSelected] = useState(0)
+    const [selected, setSelected] = useState(0)
 
-  return (
-      <div>
-        {anecdotes[selected]}
-      </div>
-  )
+    const randomAnecdote = () => {
+        const random = Math.floor(Math.random() * anecdotes.length)
+        console.log(random)
+        setSelected(random)
+    }
+
+    return (
+        <div>
+            {anecdotes[selected]}
+            <br/>
+            <button onClick={randomAnecdote}>
+                next anecdote
+            </button>
+        </div>
+    )
 }
 
 export default App
