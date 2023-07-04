391e393a98a6f83140f53af3bbd08b3780b92e56
Naveen Mittal <naveenmittal04@gmail.com>
2023-07-04 22:53:24 +0530

    phonebook step 3 done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -8,10 +8,15 @@ const App = () => {
     }
   ])
   const [newName, setNewName] = useState('')
+    const [newNumber, setNewNumber] = useState('')
 
     const handleNewNameOnChange = (event) => {
         console.log(event.target)
         setNewName(event.target.value)
+    }
+    const handleNewNumberOnChange = (event) => {
+        console.log(event.target)
+        setNewNumber(event.target.value)
     }
 
     const handleFormOnSubmit = (event) => {
@@ -25,10 +30,12 @@ const App = () => {
         const inputPerson = {
             id: (persons[persons.length-1].id + 1),
             name: newName,
+            number: newNumber
         }
         const copy = persons.concat(inputPerson)
         setPersons(copy)
         setNewName('')
+        setNewNumber('')
     };
     return (
       <div>
@@ -37,12 +44,15 @@ const App = () => {
           <div>
             name: <input value={newName} onChange={handleNewNameOnChange}/>
           </div>
+            <div>
+                number: <input value={newNumber} onChange={handleNewNumberOnChange}/>
+            </div>
           <div>
             <button type="submit">add</button>
           </div>
         </form>
         <h2>Numbers</h2>
-          {persons.map(person => <div key={person.id}>{person.name}</div>)}
+          {persons.map(person => <div key={person.id}>{person.name} {person.number}</div>)}
       </div>
   )
 }
